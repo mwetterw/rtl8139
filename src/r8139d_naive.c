3ce8170477baf0e015cc0f713ab132492ae956be
Martin Wetterwald <martin@wetterwald.eu>
2017-01-12 22:11:52 +0100

    Change module to a PCI driver module

--- src/r8139d_naive.c
+++ src/r8139d_naive.c
@@ -3,6 +3,7 @@
 #include <linux/init.h>
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
+#include <linux/pci.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Martin Wetterwald");
@@ -14,7 +15,15 @@ static struct net_device_ops r8139dn_ops =
 {
 };
 
-static int __init r8139dn_mod_init(void)
+static struct pci_device_id r8139dn_pci_id_table[] =
+{
+    {PCI_DEVICE(PCI_VENDOR_ID_REALTEK, PCI_DEVICE_ID_REALTEK_8139)},
+    {0, },
+};
+
+MODULE_DEVICE_TABLE(pci, r8139dn_pci_id_table);
+
+static int r8139dn_pci_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
     printk(KERN_INFO "rtl8139d naive driver, Hello!\n");
     r8139dn = alloc_etherdev(0);
@@ -29,12 +38,19 @@ static int __init r8139dn_mod_init(void)
     return 0;
 }
 
-static void __exit r8139dn_mod_exit(void)
+static void r8139dn_pci_remove(struct pci_dev *dev)
 {
     printk(KERN_INFO "rtl8139d naive driver, Bye!\n");
     unregister_netdev(r8139dn);
     free_netdev(r8139dn);
 }
 
-module_init(r8139dn_mod_init);
-module_exit(r8139dn_mod_exit);
+static struct pci_driver r8139dn_pci_driver =
+{
+    .name = KBUILD_MODNAME,
+    .id_table = r8139dn_pci_id_table,
+    .probe = r8139dn_pci_probe,
+    .remove = r8139dn_pci_remove,
+};
+
+module_pci_driver(r8139dn_pci_driver);
