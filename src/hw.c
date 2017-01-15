1ef71efd7c6274774e7cc9ec762f66a7d59ed603
Martin Wetterwald <martin@wetterwald.eu>
2017-01-15 20:10:35 +0100

    Avoid infinite loop if hardware fails to reset

--- src/hw.c
+++ src/hw.c
@@ -2,13 +2,20 @@
 
 void r8139dn_hw_reset ( struct r8139dn_priv * priv )
 {
+    int i = 1000;
+
     // Ask the chip to reset
     r8139dn_w8 ( CR, CR_RST );
 
-    // Wait until the reset is complete
+    // Wait until the reset is complete or timeout
     // The chip notify us by clearing the bit
-    while ( r8139dn_r8 ( CR ) & ( CR_RST ) )
+    while ( --i )
     {
+        if ( ! ( r8139dn_r8 ( CR ) & CR_RST ) )
+        {
+            // Reset is complete!
+            break;
+        }
         udelay ( 1 );
     }
 }
