7c8f63fbee5ee3eda3e7914e26254726cd253561
Martin Wetterwald <martin@wetterwald.eu>
2017-01-31 13:59:41 +0100

    Add definitions for MSR (Media Status Register)

--- src/hw.h
+++ src/hw.h
@@ -185,7 +185,16 @@ enum
     // Reserved 0x53,
     TIMERINT  = 0x54,
 
+    // Media Status Register
     MSR       = 0x58,
+        MSR_TXFCE      = ( 1 << 7 ), // TX Flow Control Enable
+        MSR_RXFCE      = ( 1 << 6 ), // TX Flow Control Enable
+        // Reserved             5
+        MSR_AUX_STATUS = ( 1 << 4 ), // Auxiliary Power Present Status
+        MSR_SPD_10     = ( 1 << 3 ), // 0: 100Mps, 1: 10Mps
+        MSR_LINKB      = ( 1 << 2 ), // 0: Link OK, 1: Link fail
+        MSR_TXPF       = ( 1 << 1 ), // 0: Sends timer done packet, 1: Sends Pause Packet
+        MSR_RXPF       = ( 1 << 0 ), // 0: Pause state clear, 1: Backoff state because Pause Packet received
 
     CONFIG3   = 0x59,
     CONFIG4   = 0x5a,
